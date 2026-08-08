d93f7103cf244ab44fdfba1677b20f2db4488861
monther hassan <hamonther90@gmail.com>
2026-08-08 21:50:10 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,48 +1,35 @@
-﻿namespace SearchTask4
+﻿namespace SearchTask4_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            List<int> numbers = new List<int>();
-
-            Console.WriteLine("How many numbers do you want to enter?");
-            int count = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter numbers:");
-
-            for (int i = 0; i < count; i++)
+            Console.Write("Enter a string: ");
+            string text = Console.ReadLine();
+            try 
             {
-                int number = Convert.ToInt32(Console.ReadLine());
-                numbers.Add(number);
-            }
-
-            try
-            {
-                foreach (int number in numbers)
-                {
-                    int counter = 0;
-
-                    foreach (int x in numbers)
-                    {
-                        if (number == x)
-                        {
-                            counter++;
-                        }
-                    }
-
-                    if (counter > 1)
-                    {
-                        throw new Exception("Duplicate number found: " + number);
-                    }
-                }
-
-                Console.WriteLine("No duplicates found.");
-            }
-            catch (Exception ex)
+                CheckVowels(text);
+                Console.WriteLine("The string contains vowels.");
+            } catch (Exception ex) 
             {
                 Console.WriteLine(ex.Message);
             }
         }
+        static void CheckVowels(string text)
+        {
+            bool hasVowel = false;
+            foreach (char letter in text)
+            { 
+                if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+                { hasVowel = true;
+                    break;
+                } 
+            } 
+            if (hasVowel == false)
+            {
+                throw new Exception("The string does not contain vowels.");
+            }
+        }
     }
 }
+  
